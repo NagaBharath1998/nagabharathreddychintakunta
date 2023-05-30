415f15ff63dfc86a73e7c806c97cefc11869d857
nagabharathchintakunta <chintakuntanagabharath22@gmail.com>
2023-05-30 12:10:19 -0500

    First upload

---
+++ indexs.java
@@ -0,0 +1,11 @@
+package nagabharathreddychintakunta;
+
+class Main {
+    public static void main(String[] args){
+
+        int a=2,b=2,c;
+        c=a-b;
+        System.out.print("sub"+ c);
+
+    }
+}
